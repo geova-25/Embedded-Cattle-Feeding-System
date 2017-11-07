8a0954e00707434ae39a7e04e8d027733cf90209
geova-25 <gjosue253@gmail.com>
2017-11-07 14:47:13 -0600

    Labels temperature humiidty and weigth

--- App/src/pages/hello-ionic/hello-ionic.ts
+++ App/src/pages/hello-ionic/hello-ionic.ts
@@ -4,8 +4,18 @@ import { Component } from '@angular/core';
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
-export class HelloIonicPage {
-  constructor() {
+export class HelloIonicPage
+{
 
+  temperatureValue: string;
+  humidityValue: string;
+  weightValue: string;
+
+  constructor()
+  {
+    this.temperatureValue = "Temperature in C";
+    this.humidityValue = "Temperature in dont know";
+    this.weightValue = "Temperature in Pounds";
   }
+
 }
