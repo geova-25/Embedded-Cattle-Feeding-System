945b3ac2ca7286cb930b8214d9fdb72d80bd1967
Sebas95 <sgonzalezquesada1@gmail.com>
2017-11-21 21:36:22 -0600

    Temperature and humedity working

--- ServerEmbebido/src/EmbeddedServer.c
+++ ServerEmbebido/src/EmbeddedServer.c
@@ -10,13 +10,95 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <wiringPi.h>
 #include "../include/softServo.h"
 #include "ServerUtils.c"
 
 #include "../include/EmbeddedServer.h"
 
+#define MAX_TIMINGS 85
+#define DHT_PIN     28   /* GPIO-20 */
 
+
+int data[5] = { 0, 0, 0, 0, 0 };
+ 
+void read_dht_data()
+{
+    uint8_t laststate   = HIGH;
+    uint8_t counter     = 0;
+    uint8_t j           = 0, i;
+ 
+    data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+ 
+    /* pull pin down for 18 milliseconds */
+    pinMode( DHT_PIN, OUTPUT );
+    digitalWrite( DHT_PIN, LOW );
+    delay( 18 );
+ 
+    /* prepare to read the pin */
+    pinMode( DHT_PIN, INPUT );
+ 
+    /* detect change and read data */
+    for ( i = 0; i < MAX_TIMINGS; i++ )
+    {
+        counter = 0;
+        while ( digitalRead( DHT_PIN ) == laststate )
+        {
+            counter++;
+            delayMicroseconds( 4 );
+            if ( counter == 255 )
+            {
+		  printf("%s  last state: %d \n","break",laststate);
+                break;
+            }
+        }
+        laststate = digitalRead( DHT_PIN );
+ 	//printf("%d \n",counter);
+        if ( counter == 255 )
+            break;
+ 
+        /* ignore first 3 transitions */
+        if ( (i >= 4) && (i % 2 == 0) )
+        {
+            /* shove each bit into the storage bytes */
+            data[j / 8] <<= 1;
+            if ( counter > 16 )
+	    {
+                data[j / 8] |= 1;
+	    }
+	//	printf(" data [ %d ] = %d  \n",j/8,data[j/8]);
+            j++;
+        }
+    }
+    //------------------------------------------------------------------------------    
+    /*
+     * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+     * print it out if data is good
+     */
+    if ( (j >= 40) &&
+	     (data[4] == ( (data[0] + data[1] + data[2] + data[3]) & 0xFF) ) )
+    {
+        float h = (float)((data[0] << 8) + data[1]) / 10;
+        if ( h > 100 )
+        {
+            h = data[0];    // for DHT11
+        }
+        float c = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
+        if ( c > 125 )
+        {
+            c = data[2];    // for DHT11
+        }
+        if ( data[2] & 0x80 )
+        {
+            c = -c;
+        }
+        
+        printf( "Humidity = %.1f %% Temperature = %.1f *C\n", h, c );
+    }else  {
+        printf( "Data not good, skip\n" );
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -215,22 +297,9 @@ void put_servo(int n)
 
     softServoSetup (0, 1, 2, 3, 4, 5, 6, 7) ;
 
+    delay(1000) ;
+    softServoWrite (0, 0);
     delay (1000) ;
-
-    softServoWrite (0,  1000) ;
-
-    delay (1000) ;
-
-    softServoWrite (0,  500) ;
-
-    delay (1000) ;
-    softServoWrite (0,  0) ;
-
-    delay (1000) ;
-
-    softServoWrite (0,  100) ;
-
-    
     send(clients[n], SUCCESS_HEADER , strlen(SUCCESS_HEADER), 0);  
 }
 
@@ -293,6 +362,18 @@ void get_variables(int n)
 	//send the success header to client
 	send(clients[n], SUCCESS_HEADER , strlen(SUCCESS_HEADER), 0);         
     short x ;
+
+
+    printf( "Raspberry Pi DHT11/DHT22 temperature/humidity test\n" );
+ 
+    if ( wiringPiSetup() == -1 )
+        exit( 1 );
+ 
+    while ( 1 )
+    {
+        read_dht_data();
+        delay( 2000 ); /* wait 2 seconds before next read */
+    }
     
     //variable where json resource is concatenated
     char jsonVariables[100] = "";
